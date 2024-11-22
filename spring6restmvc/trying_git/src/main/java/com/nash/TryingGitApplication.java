22459e735ae0e5c294c9baa5209a882604517948
Elphas_GitHub <elphasgit@gmail.com>
2024-11-22 21:11:40 +0200

    docker-push1

---
+++ spring6restmvc/trying_git/src/main/java/com/nash/TryingGitApplication.java
@@ -0,0 +1,13 @@
+package com.nash;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TryingGitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TryingGitApplication.class, args);
+	}
+
+}
