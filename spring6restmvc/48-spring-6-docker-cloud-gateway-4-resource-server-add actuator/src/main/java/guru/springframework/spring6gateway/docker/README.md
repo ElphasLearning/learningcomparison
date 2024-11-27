7074d2480000d167dc0437012b96e69730f41cc9
Elphas_GitHub <elphasgit@gmail.com>
2024-11-27 22:33:02 +0200

    27Nov

--- spring6restmvc/48-spring-6-docker-cloud-gateway-4-resource-server-add actuator/src/main/java/guru/springframework/spring6gateway/docker/README.md
+++
@@ -1,3 +0,0 @@
-# Gateway Configuration 
-
-![gateway](../images/SpringGateway.jpg)
--- spring6restmvc/48-spring-6-docker-reactive-webflux-fn-resource-server/src/main/java/com/nash/reactive/repositories/CustomerRepository.java
+++
@@ -1,7 +0,0 @@
-package com.nash.reactive.repositories;
-
-import com.nash.reactive.domain.Customer;
-import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
-
-public interface CustomerRepository extends ReactiveMongoRepository<Customer,String> {
-}
