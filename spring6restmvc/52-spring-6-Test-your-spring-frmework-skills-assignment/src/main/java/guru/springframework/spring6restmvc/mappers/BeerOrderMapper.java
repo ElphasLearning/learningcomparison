4cd345fa3679c0558caff42622229ed84f7cee91
Elphas_GitHub <elphasgit@gmail.com>
2024-12-03 00:18:48 +0200

    3Dec

---
+++ spring6restmvc/52-spring-6-Test-your-spring-frmework-skills-assignment/src/main/java/guru/springframework/spring6restmvc/mappers/BeerOrderMapper.java
@@ -0,0 +1,29 @@
+package guru.springframework.spring6restmvc.mappers;
+
+import guru.springframework.spring6restmvc.entities.BeerOrder;
+import guru.springframework.spring6restmvc.entities.BeerOrderLine;
+import guru.springframework.spring6restmvc.entities.BeerOrderShipment;
+import guru.springframework.spring6restmvc.model.BeerOrderDTO;
+import guru.springframework.spring6restmvc.model.BeerOrderLineDTO;
+import guru.springframework.spring6restmvc.model.BeerOrderShipmentDTO;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+
+@Mapper
+public interface BeerOrderMapper {
+
+   BeerOrder beerOrderDtoToBeerOrder(BeerOrderDTO beerOrderDTO);
+
+    BeerOrderDTO beerOrderToBeerOrderDto(BeerOrder beerOrder);
+
+    @Mapping(target = "beerOrder", ignore = true)
+    BeerOrderLine beerOrderLineDtoToBeerOrderLine(BeerOrderLineDTO beerOrderLineDTO);
+
+    BeerOrderLineDTO beerOrderLineTobeerOrderLineDto(BeerOrderLine beerOrderLine);
+
+    @Mapping(target = "beerOrder", ignore = true)
+    BeerOrderShipment beerOrderShipmentDtoToBeerOrderShipment(BeerOrderShipmentDTO beerOrderShipmentDTO);
+
+    BeerOrderShipmentDTO beerOrderShipmentToBeerOrderShipmentDto(BeerOrderShipment beerOrderShipment);
+
+}
